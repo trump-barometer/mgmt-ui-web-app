1f5b552f51bab29cffa7b1f073c2670cc9b3420c
d-wagner <daniel.wagner@net-z.one>
2020-07-09 21:08:25 +0200

    backend hello world working

--- backend/app.ts
+++ backend/app.ts
@@ -1,20 +1,13 @@
-import express from "express";
-import path from "path";
+import express from 'express';
+
 const app = express();
-const port = 8080; // default port to listen
+const port = 3000;
 
-// Configure Express to use EJS
-app.set( "views", path.join( __dirname, "views" ) );
-app.set( "view engine", "ejs" );
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
 
-// define a route handler for the default home page
-app.get( "/", ( req, res ) => {
-    // render the index template
-    res.render( "index" );
-} );
-
-// start the express server
-app.listen( port, () => {
-    // tslint:disable-next-line:no-console
-    console.log( `server started at http://localhost:${ port }` );
-} );
+app.listen(port, () => {
+  // tslint:disable-next-line:no-console
+  console.log(`server started at http://localhost:${port}`);
+});
