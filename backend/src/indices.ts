7b79f08d246c7343cb4b512a1bf810821c03483d
d-wagner <daniel.wagner@net-z.one>
2020-07-11 17:38:31 +0200

    basic selects working

--- backend/src/indices.ts
+++ backend/src/indices.ts
@@ -1,9 +1,19 @@
 import * as express from 'express';
+import {Response, NextFunction} from 'express';
+import { EnhancedRequest } from './lib/dbclient'
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('Hello World3');
+router.get('/', async (req: EnhancedRequest, res: Response, next: NextFunction) => {
+  const mongoClient = req.dbClient.mongoClient;
+  try {
+    const result = await mongoClient.db().collection('indizes').find({}).toArray();
+    res.json(result);
+    next();
+  } catch (e) {
+    res.json(e);
+    next();
+  }
 });
 
 
