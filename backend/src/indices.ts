7e045d16af3e2a8e048ddb05cf63b64c23745ac7
d-wagner <daniel.wagner@net-z.one>
2020-07-13 22:31:33 +0200

    try to filter only tweets which have only media, not additionally

--- backend/src/indices.ts
+++ backend/src/indices.ts
@@ -1,11 +1,11 @@
-import * as express from 'express';
-import {Response, NextFunction} from 'express';
+import * as express from 'express'
+import { Response, NextFunction } from 'express'
 import { EnhancedRequest } from './lib/dbclient'
 
-const router = express.Router();
+const router = express.Router()
 
 router.get('/', async (req: EnhancedRequest, res: Response, next: NextFunction) => {
-  const mongoClient = req.dbClient.mongoClient;
+  const mongoClient = req.dbClient.mongoClient
   try {
     const result = (await mongoClient.db()
       .collection('indizes')
@@ -14,22 +14,25 @@ router.get('/', async (req: EnhancedRequest, res: Response, next: NextFunction) 
           'indize.timestamp': 1,
           'indize.symbol': 1,
           'indize.high': 1,
-          'indize.low': 1
+          'indize.low': 1,
         },
       })
-      .sort({'indize.timestamp': 1, 'indize.symbol': 1})
+      .sort({ 'indize.timestamp': 1, 'indize.symbol': 1 })
       .toArray())
-    res.json(result.map(element => {return {
-      index: element.indize.symbol.replace('^', ''),
-      timestamp: element.indize.timestamp,
-      value: Math.round((element.indize.high + element.indize.low) / 2 * 1000) /1000
-    }}));
-    next();
+    res.json(
+      result.map(element => {
+        return {
+          index: element.indize.symbol.replace('^', ''),
+          timestamp: element.indize.timestamp,
+          value: Math.round((element.indize.high + element.indize.low) / 2 * 1000) / 1000,
+        }
+      }))
+    next()
   } catch (e) {
-    res.json(e);
-    next();
+    res.json(e)
+    next()
   }
-});
+})
 
 
-export {router as indicesRouter};
+export { router as indicesRouter }
